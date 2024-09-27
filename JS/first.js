e09b2722cfa4d652e7bd9f4a696d592e84012310
koreaIt <kory0485@gmail.com>
2024-09-27 17:42:39 +0900

    js

---
+++ JS/first.js
@@ -0,0 +1,21 @@
+console.log(this);
+
+console.log('Hello');
+// 사뿐히 즈려밟고 가시옵소서
+// console.log('나를 밟고 가라');
+console.log('World');
+
+/* 주석은 이와 같이 여러 줄에 걸쳐
+작성할 수도 있습니다.
+장문의 주석을 달거나
+
+console.log('안찍힘');
+console.log('이것도 안 찍힘');
+
+위와 같이
+코드의 일부를 주석처리하여
+비활성화는 등의 목적으로 사용되죠. */
+
+console.log('찍힘');
+
+console.log('찍힘');
